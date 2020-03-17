a5fbda402e170b0bb452bfe9e3b4673d72d4b563
rushikeshwade <waderushikesh07@gmail.com>
2020-03-17 18:24:25 +0530

    List all the Post

--- test/SocialNetwork.js
+++ test/SocialNetwork.js
@@ -25,21 +25,35 @@ contract('SocialNetwork', ([deployer, author, tipper ])=>{
     
     describe('posts', async() => {
         let result, postCount
-        it('Create Posts', async()=> {
+
+        before(async() =>{
             result = await socialNetwork.createPost('My First Post',{from:author})
-            postCount = await socialNetwork.postCount()
+            postCount = await socialNetwork.postCount()       
+        })
+        
+        it('Create Posts', async()=> {
+            
             //success
             assert.equal(postCount, 1)
             const event = result.logs[0].args
             assert.equal(event.id.toNumber(),postCount.toNumber(),'id is correct')
             assert.equal(event.content, 'My First Post','content is correct')
             assert.equal(event.tipAmount, '0','tip amount is correct')
+            assert.equal(event.author,author, 'author is correct')
+
             
             //failure
              await socialNetwork.createPost('',{from:author}).should.be.rejected
-      
-
-
             })
+        it('List all Posts', async()=> {
+            const post = await socialNetwork.posts(postCount)
+            assert.equal(post.id.toNumber(),postCount.toNumber(),'id is correct')
+            assert.equal(post.content, 'My First Post','content is correct')
+            assert.equal(post.tipAmount, '0','tip amount is correct')
+            assert.equal(post.author,author, 'author is correct')
+           
+            })
+            
+            
     })
 })        
