247e34204630f79b56f9a33212bc698f743aab19
rushikeshwade <waderushikesh07@gmail.com>
2020-04-15 16:53:51 +0530

    Updates

---
+++ precache-manifest.dcaa9227d7a457b2c7ee5a139264accd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39ab6a35e2f736281bc7",
+    "url": "/DeSocial/static/js/main.39ab6a35.chunk.js"
+  },
+  {
+    "revision": "fd36bbc2db620b4f4f4f",
+    "url": "/DeSocial/static/css/1.ad93399d.chunk.css"
+  },
+  {
+    "revision": "fd36bbc2db620b4f4f4f",
+    "url": "/DeSocial/static/js/1.fd36bbc2.chunk.js"
+  },
+  {
+    "revision": "c905676f6ad94b7e17b7",
+    "url": "/DeSocial/static/js/runtime~main.c905676f.js"
+  },
+  {
+    "revision": "0125571c448986a78d89e77ff79bb47c",
+    "url": "/DeSocial/index.html"
+  }
+];
